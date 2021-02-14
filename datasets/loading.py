14fc23b25bdca0782e287642db09b26f5e481a78
PulakIIIT <jimrohn28@gmail.com>
2021-02-14 19:14:23 +0530

    chage

--- datasets/loading.py
+++ datasets/loading.py
@@ -17,7 +17,7 @@ def load_custom_data():
     x = pickle.load(open("/content/drive/MyDrive/data/feature_matrix.pkl", "rb"))
     # y = np.array(y, dtype=int)
     x = np.array(x, dtype=float)
-    y = np.ones((1000, 1))
+    y = np.array([i % 5 for i in range(1000)])
     mean = x.mean(0)
     std = x.std(0)
     x = (x - mean) / std
