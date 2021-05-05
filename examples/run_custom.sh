3b1da660c6cfcad65dd0c5e199eafdc530cf0923
Pankil Kalra <56042783+pankilkalra@users.noreply.github.com>
2021-05-05 14:04:24 +0530

    Update run_custom.sh

--- examples/run_custom.sh
+++ examples/run_custom.sh
@@ -11,6 +11,6 @@ python train.py --dataset custom \
                 --anneal_every 10 \
                 --anneal_factor 0.5 \
                 --init_size 0.05 \
-                --num_samples 2000\
+                --num_samples 20000\
                 --rank 2 \
                 --seed 0
